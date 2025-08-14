ba018f7f7f99e0ffba9ae7c130fa327be064aff1
Elisa <193691473+ElisaDel1@users.noreply.github.com>
2025-08-14 15:35:48 +0200

    Write pseudocode for the getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+/* We need to create a function that chooses 1 string randomly between 3 strings and returns it.
+
+Create a variable computerChoice
+Create function getComputerChoice
+    Create a variable randomNumber that will receive a random number between 0 and 2
+    If randomNumber is 0, then computerChoice gets the value "rock"
+    Else if randomNumber is 1, then computerChoice gets the value "paper"
+    Else if randomNumber is 2, then computerChoice gets the value "scissors"
+    The function returns computerChoice
+*/
+
