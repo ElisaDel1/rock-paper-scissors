6a22424d90f6118986c341c24ae8e0ab24939fae
Elisa <193691473+ElisaDel1@users.noreply.github.com>
2025-08-14 16:08:17 +0200

    Put the variables in the functions scope

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 We need to create a function that chooses 1 string randomly between 3 strings and returns it.
 
-Create a variable computerChoice
 
 Create function getComputerChoice
+Create a variable computerChoice
     Create a variable randomNumber that will receive a random number between 0 and 2
     If randomNumber is 0, then computerChoice gets the value "rock"
     Else if randomNumber is 1, then computerChoice gets the value "paper"
@@ -13,9 +13,9 @@ Create function getComputerChoice
 
 */
 
-let computerChoice
 
 function getComputerChoice () {
+    let computerChoice
     let randomNumber = Math.floor(Math.random() * 3);
 
     if (randomNumber === 0) {
@@ -37,17 +37,15 @@ console.log(getComputerChoice());
 
 We want the user to input exactly either "rock", "paper" or "scissors" and store its answer in the variable humanChoice.
 
-Create a variable that will receive the user's choice humanChoice.
 
 Create a function getHumanChoice that will:
+Create a variable that will receive the user's choice humanChoice.
     Ask through a prompt the user choice and put the answer in the humanChoice variable.
 
 */
 
-let humanChoice;
-
 function getHumanChoice () {
-    humanChoice = prompt('Please choose and write "rock", "paper" or "scissors"');
+    let humanChoice = prompt('Please choose and write "rock", "paper" or "scissors"');
     return humanChoice;
 }
 
