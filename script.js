b6a71df56870b622baab953c5d700b4641e18b2c
Elisa <193691473+ElisaDel1@users.noreply.github.com>
2025-08-14 15:45:05 +0200

    Create function getComputerChoice

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ Create function getComputerChoice
     The function returns computerChoice
 */
 
+let computerChoice
+
+function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        computerChoice = 'rock';
+    } else if (randomNumber === 1) {
+        computerChoice = 'paper';
+    } else {
+        computerChoice = 'scissors';
+    };
+
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
